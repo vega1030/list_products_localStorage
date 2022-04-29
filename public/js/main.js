eb4aef8617cdbe16df9417d1b3273421383fb481
Vega Fernando <fvega.3010@gmail.com>
2022-04-30 00:19:43 +0200

    complete management of the sections app

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,11 @@ var modalcarga = new bootstrap.Modal(document.getElementById('staticBackdrop'), 
 let interfazComienzo = document.getElementById('interfaz1');
 let interfazListado = document.getElementById('interfazlistado');
 let pantallaFinal = document.getElementById('pantallafinal');
-
-
+let uiFormLogin = document.querySelector('#formLogin')
+let uiWelcome = document.querySelector ('#contentEmptyList')
+let contentModelNameUser = document.getElementById('sectionNameUser');
+let flagUiValidate = false
+let linkListUi = document.getElementById('linkViewList')
 //Captura de datos 
 
 let padre = document.getElementById('padre');
@@ -15,10 +18,8 @@ let padre = document.getElementById('padre');
 //*************SAVE NAME USER IN LOCALSTORAGE******************* */
 
 const viewNameInDom = (nameDom='')=>{
-    
+
     const contentModelWelcomeNoName = document.getElementById('sectionWelcome');
-    const contentModelNameUser = document.getElementById('sectionNameUser');
-    const viewList = document.getElementById('viewList');
   
     //html with user name
     const modelUserName = 
@@ -35,11 +36,38 @@ const viewNameInDom = (nameDom='')=>{
         contentModelNameUser.innerHTML=modelUserName;
         contentModelWelcomeNoName.style.display='none';
         contentUserNameSend.style.display = 'none';
-
+        uiWelcome.style.display = 'flex'
+        flagUiValidate = true
+        uiFormLogin.style.display = 'none'
     }
 }
 
-//delete user at localStorage
+const btnName = document.getElementById('sendNameUser').addEventListener('click', ()=>{
+    const userName = document.getElementById('nameUserInput').value;
+    console.log(localStorage.getItem('name'))
+    //validation of name and view in HTML5
+
+    if (userName === '' &&localStorage.getItem('name') === null) {
+        alert('Complete with your Name');
+        uiFormLogin.style.display='flex';
+        interfazComienzo.style.display = 'none'
+        localStorage.getItem('name');
+        
+    }
+
+    else{
+        localStorage.setItem('name', userName);
+        viewNameInDom(localStorage.getItem('name'));
+        uiFormLogin.style.display='none';
+        uiWelcome.style.display = 'flex';
+        interfazComienzo.style.display = 'flex';
+        contentModelNameUser.style.display = 'flex';
+    }
+        
+    })
+//************************ */
+
+//************* Delete user at localStorage */
 const btnclearUser = document.querySelector('#clearNameStorage').addEventListener('click',()=>{
     localStorage.removeItem('name');
     const clearNameUser = document.getElementById('nameUserInput');
@@ -47,22 +75,40 @@ const btnclearUser = document.querySelector('#clearNameStorage').addEventListene
     window.location.reload();
 })
 
-window.addEventListener('DOMContentLoaded',()=>{
-    viewNameInDom(localStorage.getItem('name'))   
+//************ Load user name when refresh page*/
 
-})
-const btnName = document.getElementById('sendNameUser').addEventListener('click', ()=>{
-    const userName = document.getElementById('nameUserInput').value;
-    const contentEmptyList = document.querySelector('#contentEmptyList')
-    // contentEmptyList.style.display='none'
-    localStorage.setItem('name', userName) 
-    viewNameInDom(localStorage.getItem('name'))   
-    })
+window.addEventListener('DOMContentLoaded',()=>{
+    viewNameInDom(localStorage.getItem('name'))
+    flagUiValidate === false? interfazComienzo.style.display = 'none':interfazComienzo.style.display = 'display'
+    
+    
+});
+
 //******************************* */
 
 
+    //View list in HTML
+
 
 //**************************** Save list in localStorage and view in html *******/
+const saveDataLocalStorage = (data='') => {
+    if(data==='' || null){
+    interfazListado.style.display = 'none'
+    return console.log('error')
+
+    }
+    else {
+        let dataLocalStorage = [];
+        dataLocalStorage = JSON.parse(localStorage.getItem('lists')) || [];
+        dataLocalStorage.push(data)
+        localStorage.setItem('lists', JSON.stringify(dataLocalStorage))
+        const dataStorageParce = JSON.parse(localStorage.getItem('lists'))
+        viewListHTML(dataStorageParce)
+
+    }
+}
+
+let dataStorageParce = JSON.parse(localStorage.getItem('lists'))
 
 
 const buttonGuardar = document.getElementById('btnguardar').addEventListener('click', ()=> {
@@ -71,43 +117,25 @@ const buttonGuardar = document.getElementById('btnguardar').addEventListener('cl
     const inputProducto = document.getElementById('inputalimento').value;
     //seleccion icono
     const seleccion = document.getElementById('seleccionicono').value;
-    seleccion === null ? alert('complete with information'): console.log('good') 
     //text area
     const textArea = document.getElementById('detalle').value;
 
-    // if(inputProducto||seleccion||textArea === ''){
-    //     alert('completa todo los campos')
-    // }
-    // else{
-    //     buttonGuardar()
-    // }
-
-    //View list in HTML
-    
-    //localStorage
     const listProducts= {
         product:inputProducto,
         select:seleccion,
         textArea:textArea
     }
-    const saveDataLocalStorage = (data) => {
-        let modeloLista = ''
-        let dataLocalStorage = [];
-        dataLocalStorage = JSON.parse(localStorage.getItem('listes')) || [];
-        dataLocalStorage.push(data)
-        localStorage.setItem('listes', JSON.stringify(dataLocalStorage))
-        const dataStorageParce = JSON.parse(localStorage.getItem('listes'))
-        console.log(dataStorageParce)
-        dataStorageParce.forEach((element, index)=>{
-            modeloLista = `<li class="list-group-item" data-producto="${element.product}" data-icono="${element.select}" data-detalle="${element.textArea}"><img src="${element.select}" alt="${inputProducto}" class="contenido3__icono">${element.product}</li>`
-            padre.innerHTML += modeloLista
-        })
-        return dataStorageParce
-    }
+    listProducts.product && listProducts.select && listProducts.textArea !=='' 
+    ? saveDataLocalStorage(listProducts): console.log('list error')
     
-    saveDataLocalStorage(listProducts)    
-    const ui1 = document.querySelector('#interfaz1')
+        
+    // window.addEventListener('DOMContentLoaded',()=>{
+    //     viewNameInDom(localStorage.getItem('name'))
+    //     const dataStorageParce = JSON.parse(localStorage.getItem('lists'))
+    //     viewListHTML(dataStorageParce)
+    //     flagUiValidate === false? interfazComienzo.style.display = 'none':interfazComienzo.style.display = 'display'
     
+    // });
     
     //reinicio modal
     document.getElementById('inputalimento').value = "";
@@ -123,43 +151,92 @@ const buttonGuardar = document.getElementById('btnguardar').addEventListener('cl
     pantallaFinal.style.display = 'none'   
 })
 
-const linkListUi = document.getElementById('linkViewList')
+//Fuction view list in HTML
+const viewListHTML = (data='') =>{
 
-localStorage.getItem('listes')!==null? linkListUi.style.display = 'flex' :
-console.log('no hay nada')
+    if(data!==null){
 
+        let modeloLista = ''
+     
+            
+        data.forEach((element, index)=>{
+            modeloLista += `
+            <li class="list-group-item" data-producto="${element.product}" data-icono="${element.select}" data-detalle="${element.textArea}" data-id="${index}"><img src="${element.select}" alt="${element.product}" class="contenido3__icono">
+            ${element.product}
+            </li>`
+            padre.innerHTML = modeloLista
+            })
+
+    }
+    }
+
+//******************************* */
+
+
+//****************** view list with </a> tag ******/ 
 linkListUi.addEventListener('click',()=>{
+ 
+
     interfazListado.style.display = 'block'
     interfazComienzo.style.display = 'none'
+    const dataStorageParce = JSON.parse(localStorage.getItem('lists'))
+    viewListHTML(dataStorageParce)
+    })
+
+//******************************* */
+
+
+
+localStorage.getItem('lists')!==null? linkListUi.style.display = 'flex' :
+linkListUi.style.display = 'none'
+//********************************/
+
+//**************** Delete Fuction */
+
+const btnDelete = document.querySelector('#btn_delete')
+.addEventListener('click', ()=>{
+
+    const btnDelete = document.querySelector('#btn_delete')
+    
+const deleted = (productId='')=>{
+    // dataStorageParce = dataStorageParce.filter(list=>list.product!=product)
+
+    dataStorageParce = dataStorageParce.filter((el,index)=>{
+        productId.indexOf(index) == -1
+    })
+    console.log(padre)
+    for(let i=0; i<padre.childNodes.length; i++){
+        console.log(padre.children[i].tagName)
+    }
+
+    deleted(productCardId)
+}
+
+ console.log(dataStorageParce)
 
 })
 
-//*****/
 
+//********************************/
 
 const llamadaFinal = document.getElementById('padre').addEventListener('click', (e)=> {
     const buttons = document.getElementById('buttons')
+    
     document.getElementById('subtitulofinal').innerHTML = e.target.getAttribute('data-producto')
     document.getElementById('iconocontenido4').src = e.target.getAttribute('data-icono')
     document.getElementById('detallefinal').innerHTML = e.target.getAttribute('data-detalle')
     pantallaFinal.style.display = 'block'
     interfazListado.style.display = 'none'
     buttons.style.display = 'block'
+    document.getElementById('btn_delete').id = e.target.getAttribute('data-id')
 
 })
 
-const cerrarPantallafinal = document.getElementById('cerrardetalle').addEventListener('click', function () {
-    pantallaFinal.style.display = 'none'
-    interfazListado.style.display = 'block'
-})
 
-const enviarDetalle = document.getElementById('enviardetalle').addEventListener('click', function () {
-    pantallaFinal.style.display = 'none'
-    interfazListado.style.display = 'none'
+
+
+const forwardButton = document.querySelector('#forwardButton').addEventListener('click', ()=>{
     interfazComienzo.style.display = 'flex'
-
-    alert('Tu listado se envio con exito')
-
-
+    interfazListado.style.display = 'none'
+    location.reload()
 })
-
